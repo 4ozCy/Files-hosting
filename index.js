f0fe0be16a17cebc6dc9515a5021bd49c8821698
Zcy. <157526994+Zyneeee@users.noreply.github.com>
2024-08-05 19:37:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function generateRandomString(length) {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '.uploads/');
+        cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
         const randomString = generateRandomString(4);
@@ -32,12 +32,12 @@ const upload = multer({
 
 app.use(express.static('public'));
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/file', upload.single('file'), (req, res) => {
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
 
-    const fileUrl = `${req.protocol}://${req.get('host')}/upload/${req.file.filename}`;
+    const fileUrl = `${req.protocol}://${req.get('host')}/file/${req.file.filename}`;
     res.json({ fileUrl });
 });
 
