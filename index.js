7a2777569dedf0b999f3af30a069e00bc03ff29d
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-10-12 00:01:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const { MongoClient, GridFSBucket } = require('mongodb');
 const path = require('path');
 const crypto = require('crypto');
 const favicon = require('serve-favicon');
-const axios = require('axios');
-const useragent = require('express-useragent');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const client = new MongoClient(process.env.MONGODB_URL, {
     useNewUrlParser: true,
@@ -53,58 +51,10 @@ const upload = multer({
 
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
-app.use(useragent.express());
 
-app.set('trust proxy', true);
-
-function sendDiscordNotification(fileDetails, req) {
-    const { filename, fileUrl, fileSize } = fileDetails || {};
-    const browserInfo = req.useragent.browser;
-    const osInfo = req.useragent.os;
-    const ipAddress = req.headers['x-forwarded-for'] || req.ip;
-    const deviceType = req.useragent.isMobile ? 'Mobile' : req.useragent.isTablet ? 'Tablet' : 'Desktop';
-
-    const locationApiUrl = `https://ipinfo.io/${ipAddress}/json?token=${process.env.TOKEN}`;
-
-    axios.get(locationApiUrl)
-        .then(response => {
-            const location = response.data;
-            const city = location.city || 'Unknown';
-            const region = location.region || 'Unknown';
-            const country = location.country || 'Unknown';
-            const locationString = `${city}, ${region}, ${country}`;
-
-            const message = {
-                content: "New file upload request received!",
-                embeds: [{
-                    title: "File Upload Request Information",
-                    fields: [
-                        { name: "Filename", value: filename, inline: true },
-                        { name: "File URL", value: fileUrl, inline: true },
-                        { name: "File Size", value: `${fileSize} bytes`, inline: true },
-                        { name: "Upload Time", value: new Date().toLocaleString(), inline: false },
-                        { name: "IP Address", value: ipAddress, inline: true },
-                        { name: "Browser", value: browserInfo, inline: true },
-                        { name: "Operating System", value: osInfo, inline: true },
-                        { name: "Device", value: deviceType, inline: true },
-                        { name: "Location", value: locationString, inline: true }
-                    ],
-                }],
-                username: 'File Upload'
-            };
-
-            axios.post(process.env.WEBHOOK, message)
-                .then(() => {
-                    console.log('Notification sent to Discord');
-                })
-                .catch((error) => {
-                    console.error('Error sending Discord notification:', error);
-                });
-        })
-        .catch(error => {
-            console.error('Error fetching location data:', error);
-        });
-}
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.post('/file', (req, res) => {
     if (!bucket) {
@@ -126,16 +76,8 @@ app.post('/file', (req, res) => {
 
         const filename = `${generateRandomString(5)}${path.extname(req.file.originalname)}`;
         const uploadStream = bucket.openUploadStream(filename);
-
         uploadStream.end(req.file.buffer, () => {
             const fileUrl = `${req.protocol}://${req.get('host')}/file/${filename}`;
-            const fileDetails = {
-                filename: filename,
-                fileUrl: fileUrl,
-                fileSize: req.file.size
-            };
-
-            sendDiscordNotification(fileDetails, req);
             res.send({ fileUrl });
         });
 
@@ -154,8 +96,8 @@ app.post('/api/file', (req, res) => {
         if (err) {
             console.error('Error during file upload:', err.message);
             if (err.code === 'LIMIT_FILE_SIZE') {
-                return res.status(400).json({ error: 'File is too large. Maximum size is 9GB.' });
-            } else if (err.message === 'File type not allowed. Only images and videos are allowed.') {
+                return res.status(400).json({ error: 'File is too large. Maximum size is 1GB.' });
+            } else if (err.message === 'File type not allowed. Only images, videos are allowed.') {
                 return res.status(400).json({ error: err.message });
             }
             return res.status(500).json({ error: 'File upload failed.' });
@@ -170,13 +112,6 @@ app.post('/api/file', (req, res) => {
 
         uploadStream.end(req.file.buffer, () => {
             const fileUrl = `${req.protocol}://${req.get('host')}/file/${filename}`;
-            const fileDetails = {
-                filename: filename,
-                fileUrl: fileUrl,
-                fileSize: req.file.size
-            };
-
-            sendDiscordNotification(fileDetails, req); // Send the notification immediately after the file is uploaded
             res.json({ fileUrl });
         });
 
@@ -198,8 +133,4 @@ app.get('/file/:filename', (req, res) => {
     downloadStream.on('end', () => {
         console.log(`File ${req.params.filename} sent successfully`);
     });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
