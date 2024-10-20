e31c12046ee8fa7b6608bfeb8d8fcab853bba9eb
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-10-20 16:39:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const { MongoClient, GridFSBucket } = require('mongodb');
 const path = require('path');
 const crypto = require('crypto');
 const favicon = require('serve-favicon');
-const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const client = new MongoClient(process.env.MONGODB_URL, {
     useNewUrlParser: true,
@@ -32,32 +31,20 @@ function generateRandomString(length) {
     return crypto.randomBytes(length).toString('hex').slice(0, length);
 }
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const uploadPath = 'uploads/';
-        if (!fs.existsSync(uploadPath)) {
-            fs.mkdirSync(uploadPath, { recursive: true });
-        }
-        cb(null, uploadPath);
-    },
-    filename: (req, file, cb) => {
-        const filename = `${generateRandomString(5)}${path.extname(file.originalname)}`;
-        cb(null, filename);
-    }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 15000 * 1024 * 1024 },
+    limits: { fileSize: 200000 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
-        const allowedTypes = /jpeg|jpg|png|gif|mp4|avi|mov|mkv|webm|wmv|hevc|h265/;
+        const allowedTypes = /jpeg|jpg|png|gif|mp4|avi/;
         const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-        const mimetype = /image\/jpeg|image\/png|image\/gif|video\/mp4|video\/avi|video\/quicktime|video\/x-matroska|video\/webm|video\/x-ms-wmv|video\/hevc/.test(file.mimetype);
+        const mimetype = allowedTypes.test(file.mimetype);
 
         if (extname && mimetype) {
             return cb(null, true);
         } else {
-            cb(new Error('File type not allowed. Only images and videos are allowed.'));
+            cb(new Error('File type not allowed. Only images, documents, videos, and archives are allowed.'));
         }
     }
 }).single('file');
@@ -77,8 +64,8 @@ app.post('/file', (req, res) => {
     upload(req, res, (err) => {
         if (err) {
             if (err.code === 'LIMIT_FILE_SIZE') {
-                return res.status(400).send('File is too large. Maximum size is 15GB.');
-            } else if (err.message === 'File type not allowed. Only images and videos are allowed.') {
+                return res.status(400).send('File is too large. Maximum size is 9GB.');
+            } else if (err.message === 'File type not allowed. Only images, documents, videos, and archives are allowed.') {
                 return res.status(400).send(err.message);
             }
             return res.status(500).send('File upload failed.');
@@ -87,33 +74,16 @@ app.post('/file', (req, res) => {
             return res.status(400).send('No file uploaded.');
         }
 
-        const filename = req.file.filename;
+        const filename = `${generateRandomString(5)}${path.extname(req.file.originalname)}`;
         const uploadStream = bucket.openUploadStream(filename);
-        const fileReadStream = fs.createReadStream(req.file.path);
-
-        fileReadStream.pipe(uploadStream).on('error', (error) => {
-            console.error('Error during file upload:', error);
-            return res.status(500).send('File upload failed.');
-        });
-
-        uploadStream.on('finish', () => {
-            fs.unlinkSync(req.file.path);
-
+        uploadStream.end(req.file.buffer, () => {
             const fileUrl = `${req.protocol}://${req.get('host')}/file/${filename}`;
             res.send({ fileUrl });
         });
-    });
-});
 
-app.get('/file/:filename', (req, res) => {
-    const downloadStream = bucket.openDownloadStreamByName(req.params.filename);
-
-    downloadStream.pipe(res).on('error', (err) => {
-        res.status(500).send('Error retrieving file.');
-    });
-
-    downloadStream.on('end', () => {
-        console.log(`File ${req.params.filename} sent successfully`);
+        uploadStream.on('error', () => {
+            res.status(500).send('File upload failed.');
+        });
     });
 });
 
@@ -124,9 +94,10 @@ app.post('/api/file', (req, res) => {
 
     upload(req, res, (err) => {
         if (err) {
+            console.error('Error during file upload:', err.message);
             if (err.code === 'LIMIT_FILE_SIZE') {
-                return res.status(400).json({ error: 'File is too large. Maximum size is 15GB.' });
-            } else if (err.message === 'File type not allowed. Only images and videos are allowed.') {
+                return res.status(400).json({ error: 'File is too large. Maximum size is 1GB.' });
+            } else if (err.message === 'File type not allowed. Only images, videos are allowed.') {
                 return res.status(400).json({ error: err.message });
             }
             return res.status(500).json({ error: 'File upload failed.' });
@@ -136,20 +107,30 @@ app.post('/api/file', (req, res) => {
             return res.status(400).json({ error: 'No file uploaded.' });
         }
 
-        const filename = req.file.filename;
+        const filename = `${generateRandomString(5)}${path.extname(req.file.originalname)}`;
         const uploadStream = bucket.openUploadStream(filename);
-        const fileReadStream = fs.createReadStream(req.file.path);
 
-        fileReadStream.pipe(uploadStream).on('error', (error) => {
-            console.error('Error during file upload:', error);
-            return res.status(500).json({ error: 'File upload failed.' });
-        });
-
-        uploadStream.on('finish', () => {
-            fs.unlinkSync(req.file.path);
-
+        uploadStream.end(req.file.buffer, () => {
             const fileUrl = `${req.protocol}://${req.get('host')}/file/${filename}`;
             res.json({ fileUrl });
         });
+
+        uploadStream.on('error', (error) => {
+            console.error('Error uploading file:', error);
+            res.status(500).json({ error: 'File upload failed.' });
+        });
+    });
+});
+
+app.get('/file/:filename', (req, res) => {
+    const downloadStream = bucket.openDownloadStreamByName(req.params.filename);
+
+    downloadStream.pipe(res).on('error', (err) => {
+        console.error('Error streaming file:', err);
+        res.status(500).send('Error retrieving file.');
+    });
+
+    downloadStream.on('end', () => {
+        console.log(`File ${req.params.filename} sent successfully`);
     });
 });
